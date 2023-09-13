78741c41c7c9e02074263881873e34825ecdb8d6
neinakada <neina.akada@ui.ac.id>
2023-09-13 10:35:23 +0700

    Add: Testcase

--- main/models.py
+++ main/models.py
@@ -4,3 +4,4 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
+    price = models.IntegerField()
--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Product
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,15 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def create_item(self):
+        self.item = Product.objects.create(
+            name        = "Nasi Putih",
+            amount      = 1,
+            description = "Nasi putih pulen",
+        )
+        return self.item
+    
+    def test_create_item(self):
+        i = self.create_item()
+        self.assertTrue(isinstance(i, Product))
